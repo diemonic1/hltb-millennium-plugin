9d2b7671886e44ddeefb45e7bdb6ca89ef57c1c4
Joey Doll <jcdoll@gmail.com>
2025-12-29 13:01:25 -0800

    removed unused webkit stuff

--- webkit/index.tsx
+++
@@ -1,5 +0,0 @@
-// Webkit entry point for Steam web pages
-// This module runs in the context of Steam web views (store, community, etc.)
-// Currently empty - HLTB functionality is in the frontend module
-
-export {};
